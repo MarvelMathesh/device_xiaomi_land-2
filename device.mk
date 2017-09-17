33a2c3fe58ce4c5c99a25c19a7eb90310de7e4e1
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    land: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff

--- device.mk
+++ device.mk
@@ -416,7 +416,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.basic_land
 
 # Vibrator
 PRODUCT_PACKAGES += \
